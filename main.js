fa40c2dd8cb6894467485c8a766f95c638f369bb
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-02-21 15:55:51 +0200

    RSVP FORM

---
+++ main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var noDisplay = document.getElementById("preference");
+    if (noDisplay.style.display === "block") {
+        noDisplay.style.display = "none";
+    } else {
+        noDisplay.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+      
+    var yesDisplay = document.getElementById("preference");
+    if (yesDisplay.style.display === "none") {
+        yesDisplay.style.display = "block";
+    } else {
+        yesDisplay.style.display = "block";
+    }
+  }
